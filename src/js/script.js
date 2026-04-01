8c49bc360771df10b88eb831836da2b641d395ca
Nayra-pixel <nayrasduarte@gmail.com>
2026-04-01 14:47:10 -0300

    feat: desenvolvendo o cadastro de vinhos.

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+//cadastro usuário
 let username = prompt('Cadastre um nome para  seu usuário:');
 if(username == ""){
     username = prompt('Por favor, digite um nome para  seu usuário:');
@@ -20,4 +21,20 @@ if(senha == ""){
 console.log(username);
 console.log(useremail);
 console.log(senha);
+
+//cadastro_vinhos
+alert('Bem-vindo ao cadastro de Vinhos! Clique em "OK" para começar!');
+let nomeVinho = prompt('Digite o nome do vinho:');
+let tipoVinho = prompt('Digite o tipo do vinho:\nTinto\nBranco\nRosé');
+let safraVinho = prompt('Digite a safra do vinho(Ano):');
+let qtdeEstoque = prompt('Digite a quantidade em estoque do vinho cadastrado:');
+
+alert("Cadastro realizado! Veja os detalhes no console.")
+
+console.log(nomeVinho);
+console.log(tipoVinho);
+console.log(safraVinho);
+console.log(qtdeEstoque);
+
+
 
